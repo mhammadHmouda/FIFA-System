d261c32766ec85316a913f358f93c93ff3506a9b
Mohammad <hmoudahamood@gmail.com>
2023-08-09 03:13:31 +0300

    Add other spring files

---
+++ BE-Apis/src/main/java/com/harri/task2/BeApisApplication.java
@@ -0,0 +1,12 @@
+package com.harri.task2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeApisApplication.class, args);
+    }
+}
---
+++ BE-Apis/src/test/java/com/harri/task2/BeApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harri.task2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
