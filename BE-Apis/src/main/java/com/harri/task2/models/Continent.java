deae098eea10413a54edca80bc5e402c057dc919
Mohammad <hmoudahamood@gmail.com>
2023-08-09 04:18:20 +0300

    Test

--- BE-Apis/src/main/java/com/harri/task2/models/Continent.java
+++ BE-Apis/src/main/java/com/harri/task2/models/Continent.java
@@ -1,18 +1,18 @@
-package com.harri.task2.models;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table(name = "continents")
-public class Continent {
-    @Id
-    private String code;
-
-    private String name;
-}
+//package com.harri.task2.models;
+//
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Entity
+//@Table(name = "continents")
+//public class Continent {
+//    @Id
+//    private String code;
+//
+//    private String name;
+//}
